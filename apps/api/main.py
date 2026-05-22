675691b2454ae6ebbe966c1327d4f2202cc340b6
Karendhaymore <karendhaymore@gmail.com>
2026-05-22 13:11:28 -0600

    Update main.py

--- apps/api/main.py
+++ apps/api/main.py
@@ -1048,6 +1048,11 @@ class UpsertMyProfilePayload(BaseModel):
     photo: Optional[str] = None
     photo2: Optional[str] = None
 
+    photo_position_x: int = 50
+    photo_position_y: int = 50
+
+    photo2_position_x: int = 50
+    photo2_position_y: int = 50
     identityPreview: Optional[str] = None
     intention: str
 
