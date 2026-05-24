d462896b4af89e02384ecd37843916ac754a1b16
Karendhaymore <karendhaymore@gmail.com>
2026-05-24 16:05:37 -0600

    Update page.tsx

--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -46,10 +46,6 @@ type FormState = {
   stateUS: string;
   photo: string;
   photo2: string;
-  photo_position_x: number;
-  photo_position_y: number;
-  photo2_position_x: number;
-  photo2_position_y: number; 
 
   relationshipIntent: string;
   datingChallenge: string;
